53164c127f1d4f178ef6153ac2a0674ce6c19da2
Manu Chandel <manuchandel@users.noreply.github.com>
2016-04-03 16:33:54 +0530

    Create CreateTable.py

---
+++ CreateTable.py
@@ -0,0 +1,15 @@
+import MySQLdb
+import nltk
+
+db = MySQLdb.connect(host="127.0.0.1", port=3306, user="root", passwd="", db="TestData",charset='utf8' )
+cursor=db.cursor()
+
+sql="""CREATE TABLE IF NOT EXISTS WikiHumans (
+  ID INT(11) NOT NULL AUTO_INCREMENT,
+  NAME VARCHAR(100) DEFAULT NULL,
+  DESCRIPTION VARCHAR(200),
+  PRIMARY KEY (ID)
+) ENGINE=InnoDB"""
+
+cursor.execute(sql)
+cursor.close()
